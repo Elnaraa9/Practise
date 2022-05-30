0e6e55798cdcf378581a0f0dc84c027231f251d2
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-05-30 11:53:51 +0400

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,3 +12,42 @@ $(".my-list li").click(function(){
       filter:selector
 })
 })
+
+let buttons=document.getElementsByClassName('add-to-card');
+[...buttons].forEach(btn=>{
+    btn.onclick=function (e) {
+        let id=this.parentElement.parentElement.getAttribute("data-id");
+        let title=this.previousElementSibling.innerText;
+        let price=this.nextElementSibling.innerHTML;
+        let img=this.parentElement.previousElementSibling.firstElementChild.getAttribute("src");
+        
+        let obj={
+            id:id,
+            title:title,
+            price: price,
+            img:img,
+            count:1
+        }
+        addBasket(obj);
+    }
+})
+
+function addBasket(obj) {
+    let basket=localStorage.getItem('basket');
+        if (basket==null) {
+            let basketArr=[obj];
+            localStorage.setItem('basket',JSON.stringify(basketArr));
+        }
+        else{
+            let basketArr=JSON.parse(basket);
+            let isExist=basketArr.find(ob=>ob.id==obj.id);
+            if(isExist===undefined){
+                basketArr.push(obj);
+            }
+            else{
+                isExist.count+=1;
+            }
+            localStorage.setItem('basket',JSON.stringify(basketArr));
+        }
+        
+}
